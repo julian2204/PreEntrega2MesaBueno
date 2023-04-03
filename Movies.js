ed330dc0a8f4e28ee7fb63f02fb6ae577395ab7c
Julian Mesa <judmesabu@gmail.com>
2023-04-03 18:36:31 -0500

    creación de clases

---
+++ Movies.js
@@ -0,0 +1,11 @@
+class Movies {
+    constructor(id, title, year, runtime, genres, director, actors) { 
+        this.id = id
+        this.title = title
+        this.year = year
+        this.runtime = runtime
+        this.genres = genres
+        this.director = director
+        this.actors = actors
+    }
+}
